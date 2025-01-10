2862221b8a84894781f5aab66968069ae7e533fe
Isak Haugen Sørheim <isak@paneda.no>
2025-01-10 08:46:27 +0100

    Tested get and post functions, add admin, store and profile page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Test");
+import express, {Request, Response} from "express";
+
+const info: string[] = [];
+
+const app = express();
+app.use(express.json());
+
+app.get("/api/info", (req: Request, res: Response<string[]>) => {
+    res.status(200).json(info);
+});
+
+app.post("/api/info", (req: Request, res: Response) => {
+    info.push(req.body)
+    console.log(info);
+    res.status(200);
+})
+app.listen(8080, () => {
+    console.log("start");
+
+})
---
+++ frontend/src/app/app.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+  constructor(private http: HttpClient) {}
+
+  getInfo = () => {
+    return this.http.get('/api/info');
+  }
+
+  postInfo = (info:string) => {
+    return this.http.post<string>('/api/info', 'test');
+  }
+}
