f194e67c4c682b6a2b2fc811bfd4dd204f1b7f24
Isak Haugen Sørheim <isak@paneda.no>
2025-01-17 11:40:23 +0100

    Added put path in backend, implemented update functionality in store page

--- frontend/src/app/store-page/store-page.component.ts
+++ frontend/src/app/store-page/store-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {StorePageService} from "./store-page.service";
 
 @Component({
   selector: 'app-store-page',
@@ -9,5 +10,15 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./store-page.component.scss']
 })
 export class StorePageComponent {
+  user: any;
 
+  constructor(private service: StorePageService) {
+    this.user = JSON.parse(localStorage.getItem('loggedInUser')||'{}');
+  }
+
+  purchase(item: {productName: string, productPrice: number}) {
+    this.user.orderHistory.push(item);
+
+    this.service.updateUser(this.user._id, this.user).subscribe();
+  }
 }
