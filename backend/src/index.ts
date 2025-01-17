4f0aeed5cdf8f16829785f96e73be988b9c9acfb
Isak Haugen Sørheim <isak@paneda.no>
2025-01-17 09:19:44 +0100

    Implemented login path in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,6 +17,12 @@ app.get("/api/users", async (req: Request, res: Response) => {
     res.status(200).json(users);
 });
 
+app.post("/api/login", async (req: Request, res: Response) => {
+    const user = await User.find(req.body);
+
+    res.status(200).json(user);
+})
+
 app.post("/api/info", async (req: Request, res: Response) => {
     const {firstName, lastName, userName, password} = req.body;
 
