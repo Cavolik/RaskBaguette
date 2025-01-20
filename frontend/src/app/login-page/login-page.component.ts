bb7a4c028b58cf1c33972489ce64fc8cb37c66c7
Isak Haugen Sørheim <isak@paneda.no>
2025-01-20 15:54:07 +0100

    Improved styling of login page, removed formbuilder

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,27 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import { LoginPageService } from "./login-page.service";
-import {RouterLink} from "@angular/router";
+import { RouterLink } from "@angular/router";
+import { MatInputModule } from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, MatInputModule, MatButtonModule],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
-  loginForm: FormGroup;
-  constructor(private fb: FormBuilder, private service: LoginPageService) {
-    this.loginForm = this.fb.group({
-        userName: ['', Validators.required],
-        password: ['', Validators.required]
-      });
-  }
+  loginForm = new FormGroup({
+    userName: new FormControl({ value: '', disabled: false }, { nonNullable: true, validators: [Validators.required]}),
+    password: new FormControl({ value: '', disabled: false }, { nonNullable: true , validators: [Validators.required]}),
+  })
+  constructor(private service: LoginPageService) {}
 
   logIn() {
-    this.service.logIn(this.loginForm.value).subscribe(user => {
+    this.service.logIn(this.loginForm.getRawValue()).subscribe(user => {
       const userArray = user as Array<any>;
       if (userArray.length > 0) {
         localStorage.clear();
