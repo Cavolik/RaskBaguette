dcb6712fa8647c75a31d502bb3315725831c6b67
Thomas Flølo <thomas@paneda.no>
2025-01-27 14:18:01 +0100

    wrote some tests to verify login path, the third test fails, see if you can fix the problem (in the app not in the test). Hint cookie name in session config (search tip: how to set the name of my cookie in express-session options)

---
+++ backend/src/__tests__/backend.tests.ts
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import { app } from '../index';
+
+describe('Backend tests', () => {
+  describe('/api/login', () => {
+    describe('POST', () => {
+      it('should login to the app', async () => {
+        const response = await request(app)
+          .post('/api/login')
+          .send({ username: 'test', password: 'test' });
+        expect(response.statusCode).toBe(200);
+      });
+
+      it('should return 401 when wrong password', async () => {
+        const response = await request(app)
+          .post('/api/login')
+          .send({ username: 'test', password: 'wrong' });
+        expect(response.statusCode).toBe(401);
+      });
+
+      it('should contain Set-Cookie in response.headers when successfully logged in', async () => {
+        const response = await request(app)
+          .post('/api/login')
+          .send({ username: 'test', password: 'test' });
+        console.log(response.headers['set-cookie']);
+        expect(response.headers['set-cookie']).toBeDefined();
+      });
+
+      it('should return 200 and set-cookie header should contain backendsession', async () => {
+        const response = await request(app)
+          .post('/api/login')
+          .send({ username: 'test', password: 'test' });
+        const setCookieHeader: string = response.headers['set-cookie'][0];
+        console.log(setCookieHeader);
+        expect(setCookieHeader.split('=')[0]).toBe('backendsession');
+      });
+    });
+  });
+});
