692aef2005642188d5cc0abeb5065c1a47ad38fb
Isak Haugen Sørheim <isak@paneda.no>
2025-01-29 11:41:43 +0100

    Changed user to await find one, removed temporary database array

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -50,21 +50,6 @@ app.get('/api/users', async (req: Request, res: Response) => {
   res.status(200).json(users);
 });
 
-const userDatabase = [
-  {
-    id: 'id1',
-    username: 'test',
-    password:
-      '8Gnhiz3mhf2YrPx/ws+fnpvmUVyi0RSp7AmDGL/aui4P+9CUhEmIzPK0IaQtuhdjWaYJDdRtvutjXKPar/oeasD55KZRCKFMkTLFQjs5N90fsBe5o6EOJ9DXxhgLQc/UOAjpeth9cHiOy3NxpL8qgWTgCWZRTWk9grzHD5y0+cE=:gKvdYWujo142pvoRqBtgUHzyo5r9kKkVb7VdbdtCFRSxYb4ZpcOQtanLFz1c9sCrYk3E2SI3sJabPJN16ZmZsF0hD2HJJjDIER9LoZx4HQZoBo/F4f0NLoc1kuHq+IrM2kkoYrBIQFA1umExAl7pUuKXsBcJCZU7n8ix+6kgJhSd3yp6I4vKNOMiQDjyBuQTil1FLaW1mxOYZ1rttS6zlaGp93jWLqxjps59/UsQMO2MRFetGPPRzKhBnYE3iT52Irw2YGqs2rg1VA7MvxT65hQmsmsEcE74Jg9glMe53RW+ETqmCDjD2lPfLPLVQWq6CT+X5aCsZzaDxEm4tkjPDtb6f8hH5fx+PnwtLK4HW12iO0GoCKEzcK4qetEBSjKjms6yfJFUKIR6g4hV2JTEwbLFAJ6HpVS7SpY00l27zQPelqgXFIJcU+eyPY2i+uyG+y/D6bToCP8OQ6c2MpXCd/a9hS/30rcHrJJMdZr+YBwlIPZyjcF1Wl/iLp5KGaRYhNtICaOhWGYybgVA/Ov4RLnNRLBaRsNfLKR4QSxGFDpNBVd/RyJ0NWvpo8tNJkYO6bcvKJtXnVLa7rqT2Pw864z3wyguk9X0OPGhgNzrDdUIrUyBTvNI/vDe+oIb5h7dodlZUb5QFU9eX8MhcA6PuffKbxk82nAodgnexCEua2c=',
-  },
-  {
-    id: 'id2',
-    username: 'test2',
-    password:
-      'F3p/aAZOfgG6EqaZwSIz7mKO5zw34dxUnap78CZa+UbF5/EyYL5WJy5XiXd7d5paOREsLHj1ab5SV9f6sAi5Gl8yWZerbCl+LaP3ufhjZxAe5Sgoo7MFY1R1pFmzKxebhrd3ahLsLHerGjMEo9NaDwOsuOUGdLMofo7PLurZJj0=:cnufLXQxebBbF6AAw7ct5//n6+EbZFNSCr2wARsluQ1XeGdw9BQI08d5SX3ko4olnSEAybdiUq1ROLss4XSZmbrr4TRpYG2agrrUgekMJdVtumjSSkyCHE9OEpIZ1eOA/KaojCEZRN4UCuERWbW6GVmevm0b4Z78sSXjkBQ2eBX/YxrIyd2MIuI/ES4KntqnhyxtO6xxIplQvfVJrRO//ZzjDby5jggO7a4rSzxNKT78GY8zT5RQ6H+ma71D44UttZMLDuYb+2NOtyaQO67qKKiw0kPAtSCf7E2IK1XrjyLvOvToXXpAebkRhrT6vGruRJDUUqISn7AR6byiPOBe3Kt2epZl0gfzY00c7KZTM2BDtIwEJPinptUHIKwtncpuFxU4Iahy8dGTSiiKVD8VHWAmg7LukWG5kXBf0++9/X9Pdk3BVZGtQcbBsnA1malKTJ50IDZDA3a1eTHZEaF9Ta1tSH1/iONA3EJ3uAwIa99r2dFnOLv8NzdhWlNJjzp2ILIJxQR6HBXxOyan6HMa9FIlaVxbSmnkI9dx1TVOztkqZGE9CozY1pIbBqXJlK4Bmj9HGvsDDmdIReP/HeU8CnShoGhH0zqtA+re4OgbPIuaAXfOKzP2/npG9XkmlGVjL00zCpUUZMCkNeIUJG/m8I7Moqnz511uzeDpPDP/eNo=',
-  },
-];
-
 declare module 'express-session' {
   interface SessionData {
     userId: string;
@@ -76,7 +61,7 @@ app.post('/api/login', async (req: Request, res: Response, next: NextFunction) =
   try {
     const { username, password } = req.body; // we extract username and password from the request body. We have typed the request object and specified how the request body looks.
     //find the user
-    const user = userDatabase.find((user) => user.username === username);
+    const user = await User.findOne({userName: username});
     if (!user || !verifyHashedPassword(password, user.password)) {
       //if no user is found or the user.password is not equal to password in the body, we return 401
       return res.status(401).json({ msg: 'Unauthorized' });
