31ede882f2cbb8bd7d995bdbb7853a5fadd8b762
Isak Haugen Sørheim <isak@paneda.no>
2025-02-04 12:49:36 +0100

    Added admin user to database if no users exists

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import { app } from './app';
 import { connectToMongoDB } from './database';
+import { User } from "./models/user";
+import { hashPassword } from "./utils/authUtils";
+
 
 void (async function startup() {
   try {
     await connectToMongoDB();
+    const exists = await User.exists({userName: 'admin'});
+    if(exists === null){
+      void (await User.create({firstName: 'Admin', lastName: 'User', userName: 'admin', password: hashPassword('admin')}));
+    }
     app.listen(8080, () => {
       console.log('Server is running on port 8080');
     });
