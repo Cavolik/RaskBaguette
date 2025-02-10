4a831033cf5c98da5d7c79f388fc5da989102393
Isak Haugen Sørheim <isak@paneda.no>
2025-02-10 13:25:26 +0100

    Started implementing http interceptor

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {provideHttpClient} from "@angular/common/http";
+import { HTTP_INTERCEPTORS, provideHttpClient } from "@angular/common/http";
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpApiInterceptor } from "./http-api.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations(), { provide: HTTP_INTERCEPTORS, useClass: HttpApiInterceptor, multi: true },]
+
 };
---
+++ frontend/src/app/http-api.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { catchError, Observable, throwError } from "rxjs";
+import { Router } from "@angular/router";
+
+
+@Injectable()
+export class HttpApiInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {
+  }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next
+      .handle(req)
+      .pipe(catchError((err: HttpErrorResponse) => this.handleApiError(err)));
+  }
+
+  private handleApiError(res: HttpErrorResponse) {
+    if (res.status === 401) {
+      console.log('401');
+      if (!(this.router.url === '/login')) {
+        this.router.navigate(['/login']);
+      }
+    }
+    console.log(new Date());
+    return throwError(() => res);
+  }
+}
